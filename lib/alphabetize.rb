42cc04dea22d35c5600c5d3c67db6408ccd96d72
Elma Rahman <elmarahman635@gmail.com>
2019-05-05 01:40:07 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,10 +1,34 @@
 require 'pry'
 
-def alphabetize(arr)
-arr.sort_by do |word|
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz" #this is in the global scope, so ruby will check for it if it doesnt find it in the block
 
-#binding.pry
+
+def alphabetize(arr)
+    arr.sort_by do |word|
+      word.split('').map do |letter|
+    ESPERANTO_ALPHABET.index(letter) #searches for letter's value in that variable & looks for the position. 
+    end
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # - should use char
 # - should use map
+
+#alpha based on first  letter , second letter, sixth  letter, second letters not in English alphabet
