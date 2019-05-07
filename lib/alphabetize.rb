eed2e9a4d91bc4b786b6a876330abe31a5a24762
Constantine Chutis <constantinec84@gmail.com>
2019-05-07 16:46:27 -0400

    Finished

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
 def alphabetize(arr)
   # code here
+  esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split('')
+  arr.sort_by do |word|
+    word.split('').collect do |letter|
+      esperanto.index(letter)
+    end
+  end
 end
